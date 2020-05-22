deca039ce405360746eac354ebe75b1a5bc5f4de
g-batalhao-a <al.darkseidd.2010@gmail.com>
2020-05-22 13:59:33 +0100

    Done

---
+++ Entrega3/gatilho1_verifica.sql
@@ -0,0 +1,3 @@
+SELECT * from produto;
+
+INSERT into quantidade VALUES(594887697,123456789,10000);
---
+++ Entrega3/gatilho2_verifica.sql
@@ -0,0 +1,5 @@
+select * from quantidade natural join produto;
+
+INSERT INTO quantidade VALUES(646430620, 531978268, 100);
+
+select * from quantidade natural join produto;
